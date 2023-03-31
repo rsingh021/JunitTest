d815340f21ec81cf215b20dc2983141272a3018d
ravjot <ravjotsingh1434@gmail.com>
2023-03-30 23:45:52 -0400

    Initial commit

---
+++ src/PrimeFactors/PrimeFactors.java
@@ -0,0 +1,15 @@
+package PrimeFactors;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PrimeFactors {
+        public List<Integer> generate(int num){
+            List<Integer> primes = new ArrayList<Integer>();
+            for (int candidate = 2; num > 1; candidate++)
+                for(; num%candidate == 0; num/=candidate)
+                    primes.add(candidate);
+            return primes;
+
+    }
+}
---
+++ src/PrimeFactors/PrimeFactorsTest.java
@@ -0,0 +1,54 @@
+package PrimeFactors;
+import PrimeFactors.PrimeFactors;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class PrimeFactorsTest {
+   PrimeFactors pf;
+
+    @BeforeEach
+    public void setup() {
+        pf = new PrimeFactors();
+    }
+    public List<Integer> list(int... ints) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i : ints)
+            list.add(i);
+        return list;
+    }
+    @Test
+    public void testOne() throws Exception {
+        assertEquals(list(), pf.generate(1));
+    }
+    @Test
+    public void testTwo() throws Exception {
+        assertEquals(list(2), pf.generate(2));
+    }
+    @Test
+    public void testThree() throws Exception {
+        assertEquals(list(2,3), pf.generate(4));
+    }
+    @Test
+    public void testFour() throws Exception {
+        assertEquals(list(2,2), pf.generate(4));
+    }
+    @Test
+    public void testSix() throws Exception {
+        assertEquals(list(2,3), pf.generate(6));
+    }
+    @Test
+    public void testEight() throws Exception {
+        assertEquals(list(2,2,2), pf.generate(8));
+    }
+    @Test
+    public void testNine() throws Exception {
+        assertEquals(list(3,3), pf.generate(9));
+    }
+}
